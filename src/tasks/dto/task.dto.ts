1f231b648e4ee77908aec8fa8439d40b0e6b7ae0
dackmagic115 <pakaphon23@gmail.com>
2020-04-06 00:43:44 +0700

    feat: validate

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,9 +1,20 @@
+import { IsNotEmpty, IsOptional, IsEnum } from 'class-validator';
+import { TaskStatus } from '../services/tasks.service';
+
 export class CreateTaskDTO {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
 
-export interface FilterDTO {
-  status: string;
+// tslint:disable-next-line: max-classes-per-file
+export class FilterDTO {
+  @IsOptional()
+  @IsEnum(TaskStatus)
+  status: TaskStatus;
+
+  @IsOptional()
   search: string;
 }
