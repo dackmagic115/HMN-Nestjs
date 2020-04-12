7be48f7b7e845945949cccae0972dc8dcd2e7ea9
dackmagic115 <pakaphon23@gmail.com>
2020-04-12 22:23:18 +0700

    feat: getone with Database

--- src/tasks/controllers/tasks.controller.ts
+++ src/tasks/controllers/tasks.controller.ts
@@ -8,41 +8,41 @@ import {
   Patch,
   Query,
 } from '@nestjs/common';
-import { TasksService, ITask, TaskStatus } from '../services/tasks.service';
+import { TasksService } from '../services/tasks.service';
 import { CreateTaskDTO, FilterDTO } from '../dto/task.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private service: TasksService) {}
 
-  @Get()
-  getAllTasks(@Query() filterDTO: FilterDTO): ITask[] {
-    if (Object.keys(filterDTO).length) {
-      return this.service.getAllWithFilters(filterDTO);
-    }
-    return this.service.getAllTasks();
-  }
+  //   @Get()
+  //   getAllTasks(@Query() filterDTO: FilterDTO): ITask[] {
+  //     if (Object.keys(filterDTO).length) {
+  //       return this.service.getAllWithFilters(filterDTO);
+  //     }
+  //     return this.service.getAllTasks();
+  //   }
 
-  @Post()
-  createTask(@Body() dto: CreateTaskDTO): ITask {
-    return this.service.createTask(dto);
-  }
+  //   @Post()
+  //   createTask(@Body() dto: CreateTaskDTO): ITask {
+  //     return this.service.createTask(dto);
+  //   }
 
   @Get(':id')
   getTaskById(@Param('id') id: string) {
     return this.service.getTaskById(id);
   }
 
-  @Delete(':id')
-  deteleTask(@Param('id') id: string) {
-    return this.service.deleteTask(id);
-  }
+  //   @Delete(':id')
+  //   deteleTask(@Param('id') id: string) {
+  //     return this.service.deleteTask(id);
+  //   }
 
-  @Patch('/:id/status')
-  updateTaskStatus(
-    @Param('id') id: string,
-    @Body('status') status: TaskStatus,
-  ) {
-    return this.service.updateTaskStatus(id, status);
-  }
+  //   @Patch('/:id/status')
+  //   updateTaskStatus(
+  //     @Param('id') id: string,
+  //     @Body('status') status: TaskStatus,
+  //   ) {
+  //     return this.service.updateTaskStatus(id, status);
+  //   }
 }
