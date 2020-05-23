ac29330dcbb7892b15c611c2d54dc535ba08b951
dackmagic115 <pakaphon23@gmail.com>
2020-05-23 22:34:08 +0700

    feat: add guard

--- src/tasks/controllers/tasks.controller.ts
+++ src/tasks/controllers/tasks.controller.ts
@@ -10,35 +10,42 @@ import {
   ParseIntPipe,
   Put,
   ValidationPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { TasksService } from '../services/tasks.service';
 import { CreateTaskDTO, FilterDTO } from '../dto/task.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private service: TasksService) {}
 
   @Get()
+  @UseGuards(AuthGuard())
   getAll(@Query(ValidationPipe) FilterDTO: FilterDTO) {
     return this.service.getAll(FilterDTO);
   }
 
   @Post()
+  @UseGuards(AuthGuard())
   createTask(@Body() dto: CreateTaskDTO) {
     return this.service.insertEntry(dto);
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard())
   getTaskById(@Param('id', ParseIntPipe) id: number) {
     return this.service.getTaskById(id);
   }
 
   @Put(':id')
+  @UseGuards(AuthGuard())
   update(@Body() dto: CreateTaskDTO, @Param('id', ParseIntPipe) id: number) {
     return this.service.updateEntry(id, dto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard())
   deteleTask(@Param('id', ParseIntPipe) id: number) {
     return this.service.deleteEntry(id);
   }
